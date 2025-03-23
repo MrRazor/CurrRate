92ae82e56f76d816834f003e0c168a28e3f8705f
MrRazor <jantomasstekl@gmail.com>
2025-03-23 18:46:11 +0100

    remove comments

--- app/src/main/java/cz/razor/currrate/workers/NotificationWorker.kt
+++ app/src/main/java/cz/razor/currrate/workers/NotificationWorker.kt
@@ -7,17 +7,14 @@ import cz.razor.currrate.helpers.NotificationHelper
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class NotificationWorker(appContext: Context, workerParams: WorkerParameters) : //dědíme z abstraktní třídy Worker, který příjímá parametry typu Context a WorkerParameters
-    Worker(appContext, workerParams), KoinComponent{ //jelikož chceme získat instanci NotificationHelper pomocí Koin, musíme implementovat rozhraní KoinComponent
+class NotificationWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams), KoinComponent{
 
     private val notificationHelper: NotificationHelper by inject()
 
-    override fun doWork(): Result { //metoda abstraktní třídy Worker, kterou je nutné implementovat
-        //zde typicky zkontrolujete nějaký stav, a pokud se splní dané podmínky zobrazí se notifikace, pokud ne tak nic neprovádíme
-        //my pro účely ukázky posíláme notifikaci vždy
+    override fun doWork(): Result {
         notificationHelper.showNotification("CryptoApp", "Nezapomeň zkontrolovat ceny kryptoměn!")
 
-        // Vracíme Result.success(), pokud se úloha provedla úspěšně
         return Result.success()
     }
 }
